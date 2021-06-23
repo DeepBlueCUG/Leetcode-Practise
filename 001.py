c19cf10a230850c6298dd56fc24e10b15de92d75
rsliuxk <rsliuxk@yeah.net>
2021-06-23 17:30:17 +0800

    quesrion 1 for leetcode

---
+++ 001.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSum(self, nums: list, target) -> list:
+
+        result = []
+        map = {}
+
+        for i in range(len(nums)):
+            map[nums[i]] = i
+        for j in range(len(nums)):
+            diff = target - nums[j]
+            if map.__contains__(diff) and map.get(diff) != j:
+                result.append(j)
+                result.append(map.get(diff))
+                return result
+
+        return result
+
+solution_test = Solution()
+result_test = solution_test.twoSum([2, 7, 11, 15], 9)
+print(result_test)
