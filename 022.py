d7a3318a8bf0c4e07198ece0504d67ad49c9afd8
rsliuxk <rsliuxk@yeah.net>
2021-08-27 16:48:35 +0800

    022_括号生成_回溯练习3

---
+++ 022.py
@@ -0,0 +1,28 @@
+# 22题能否用用回溯完成呢
+# 是不是有效的括号组合，可以通过左右括号来判定，左括号数量一定大于右括号
+class Solution:
+    def generateParenthesis(self, n: int) -> list:
+
+        result = []
+        temp = ''
+        left = 0
+        right = 0
+        self.__recall(n, right, left, temp, result)
+        return result
+
+    def __recall(self, num, left, right, temp, result: list):
+        if len(temp) == 2 * num:
+            result.append(temp[:])
+            return
+        if left < num:
+            temp = temp + '('
+            self.__recall(num, left + 1, right, temp, result)
+            temp = temp[:-1]
+        if right < left:
+            temp = temp + ')'
+            self.__recall(num, left, right + 1, temp, result)
+            temp = temp[:-1]
+
+solution = Solution()
+result = solution.generateParenthesis(3)
+print(result)
